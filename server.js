2563e0880922736c7d9e296c4c9a23b00a9a6e59
Harshit <raj.harshitsinha08@gmail.com>
2025-05-15 11:17:11 +0530

    adding csp headers

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Middleware to set Content Security Policy headers
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; " +
+      "font-src 'self' https://medigo-frontend.vercel.app; " +
+      "script-src 'self'; " +
+      "style-src 'self' 'unsafe-inline'; " +
+      "img-src 'self';"
+  );
+  next();
+});
 
 // Serve static files from the current directory
 app.use(express.static(path.join(__dirname)));
 
 // Serve index.html for the root route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
