d1589554a32cdfc4e1ed79996098a73a0970b2e4
Harshit <raj.harshitsinha08@gmail.com>
2025-05-15 11:24:19 +0530

    add login route

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
